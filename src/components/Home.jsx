ff88a82d4d78ba4668ab794a389470434418346a
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-02-03 16:40:50 +0530

    adding delete functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,62 +1,71 @@
 import axios from 'axios';
-import { useEffect ,useState } from 'react';
+import { useEffect, useState } from 'react';
 import User from './User';
 import style from '../styles/user.module.css';
 
 const Home = () => {
+  const [adding, setAdding] = useState(false);
+  const [data, setData] = useState([]);
+  async function fetchApi() {
 
-  const [adding,setAdding] =useState(false);
-  const [data , setData] =useState([]);
-  async function fetchApi(){
-    
-    let getdata =await axios.get('https://jsonplaceholder.typicode.com/albums');
+    let getdata = await axios.get('https://jsonplaceholder.typicode.com/albums');
     console.log(getdata)
     setData(getdata.data);
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchApi();
-  },[])
+  }, [])
 
-  const setUserID =(e) =>{
+  const setUserID = (e) => {
     console.log(e);
   }
-  const setTitle = e =>{
+  const setTitle = e => {
     console.log(e);
   }
-  
+
+  const removeAlbum = (id) => {
+
+    const newList = data.filter(album => id !== album.id)
+    setData(newList);
+  }
+
+  const addAlbum = (id) => {
+    
+  }
+
   return (
     <div >
-    <div className={style.addItem}>
+      <div className={style.addItem}>
         {
           adding &&
-            < >
+          < >
             <div className={style.formGroup}>
-                <span className={style.usertitle}>UserID</span>
-                <input className={style.formfield} type="text" onChange={(e)=>setUserID(e.target.value)}/>
+              <span className={style.usertitle}>UserID</span>
+              <input className={style.formfield} type="text" onChange={(e) => setUserID(e.target.value)} />
             </div>
 
             <div className={style.formGroup}>
-                <span className={style.usertitle}>Title</span>
-                <input className={style.formfield} type="text" onChange={(e)=>setTitle(e.target.value)}/>  
+              <span className={style.usertitle}>Title</span>
+              <input className={style.formfield} type="text" onChange={(e) => setTitle(e.target.value)} />
             </div>
 
-                <input  type="submit"  onClick={()=>setAdding(false)}/>
-            </>  
+            <input type="submit" onClick={() => setAdding(false)} />
+          </>
         }
         {
-          !adding && <button onClick={()=>setAdding(true)} className={style.buttonAdd} type="submit">Click Here To Add A New Album</button>
-          
+          !adding && <button onClick={() => setAdding(true)} className={style.buttonAdd} type="submit">Click Here To Add A New Album</button>
+
         }
-        </div>
-        <div className={style.alluser}>
+      </div>
+      <div className={style.alluser}>
         {
-          data.map((item)=>{
-            return <User data={item} key={item.id}/> 
+          data.map((item) => {
+            return <User data={item} key={item.id} removeAlbum={removeAlbum}/>
           })
         }
-        </div>
-        
+      </div>
+
 
     </div>
   )
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import style from '../styles/user.module.css';
-const User = (data) => {
-    console.log(data,"dataaaa");
-    const {id , title  } =data.data;
+const User = (props) => {
+  
+  const {data,removeAlbum} =props
+  
+  const { id, title } = data;
   return (
-        
-        <div className={style.container}>
-            <div className={style.singlePost}>
-            <span>{id}</span>.
-            <span className={style.userTitle}>{title}</span>
-            </div>
-            <div className={style.btn}>
-            <button className={style.btnEdit}>Edit</button>
-            <button className={style.btnDelete}>Delete</button>
-            </div>
-        </div>
-            
-        
-        
+
+    <div className={style.container}>
+      <div className={style.singlePost}>
+        <span>{id}</span>.
+        <span className={style.userTitle}>{title}</span>
+      </div>
+      <div className={style.btn}>
+        <button className={style.btnEdit}>Edit</button>
+        <button onClick={e => removeAlbum(id)} className={style.btnDelete}>Delete</button>
+      </div>
+    </div>
+
+
+
   )
 }
 
